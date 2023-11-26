f3106bc02106319272b5b88fee68200bc60cefc3
Mr.Blinky <mstr.blinky@gmail.com>
2023-11-26 13:33:49 +0100

    Add Stella support
    Add code to optionally Launch the Stella emulator.
    
    Update readme how to use Stella emulator.

--- dump2600p.py
+++ dump2600p.py
@@ -1,5 +1,5 @@
 ################################################################################
-#  Atari 2600+ Dumper v1.00                            by Mr. Blinky Nov 2023  #
+#  Atari 2600+ Dumper v1.01                            by Mr. Blinky Nov 2023  #
 ################################################################################
 
 # requires Python 3 & pyserial to be installed
@@ -10,6 +10,7 @@ import serial
 import os.path
 import time
 import hashlib
+import subprocess
 
 romdir = os.path.split(sys.argv[0])[0] + os.sep + 'romdumps' + os.sep
 
@@ -38,30 +39,45 @@ def read(n):
   return bytes
 
 print('Waiting for dumper to respond...(Remove and/or insert cart)')  
-while True:
-  cmd = read(1)
-  if cmd[0] == 0xEA:
-    print('No cart detected')
-  #expect rom dump command
-  elif cmd[0] == 0xAA:
-    cmd += read(4)
-    if len(cmd) == 5 and cmd[1] == 0x55 and cmd [2] == 0x26:
-      romsize = ((cmd[4] >> 4) * 10 + (cmd[4] & 0x0f)) * 1024
-      print('Reading {} bytes from cart'.format(romsize))
-      romdump = read(romsize)
-      romhash = hashlib.md5(romdump).hexdigest()
-      #expect cart type command
-      cmd = read(5)
-      if cmd[0] == 0x55 and cmd[1] == 0xAA:
-        romtype = cmd[3] * 256 + cmd[4]
-        print('Detected cart type 0x{:04X}'.format(romtype))
-        #save romdump
-        filename = romdir + "romdump-{}-md5-{}-type-{:04x}.bin".format(time.strftime("%Y%m%d-%H%M%S", time.localtime()), romhash, romtype)
-        
-        print('Saving romdump to ' + filename)
-        if not os.path.exists(romdir):
-          os.makedirs(romdir)
-        with open(filename, "wb") as binfile:
-          binfile.write(romdump)
-          binfile.close()
-        print('Remove cart and insert next cart to dump\n')
+process = None
+try:
+  while True:
+    cmd = read(1)
+    if cmd[0] == 0xEA:
+      print('No cart detected')
+    #expect rom dump command
+    elif cmd[0] == 0xAA:
+      cmd += read(4)
+      if len(cmd) == 5 and cmd[1] == 0x55 and cmd [2] == 0x26:
+        romsize = ((cmd[4] >> 4) * 10 + (cmd[4] & 0x0f)) * 1024
+        print('Reading {} bytes from cart'.format(romsize))
+        romdump = read(romsize)
+        romhash = hashlib.md5(romdump).hexdigest()
+        #expect cart type command
+        cmd = read(5)
+        if cmd[0] == 0x55 and cmd[1] == 0xAA:
+          romtype = cmd[3] * 256 + cmd[4]
+          print('Detected cart type 0x{:04X}'.format(romtype))
+          #save romdump
+          filename = romdir + "romdump-{}-md5-{}-type-{:04x}.bin".format(time.strftime("%Y%m%d-%H%M%S", time.localtime()), romhash, romtype)
+          
+          print('Saving romdump to ' + filename)
+          if not os.path.exists(romdir):
+            os.makedirs(romdir)
+          with open(filename, "wb") as binfile:
+            binfile.write(romdump)
+            binfile.close()
+          #launch with stella
+          if len(sys.argv) > 2:
+            s = "Launching stella using:"
+            args = []
+            for i in range(2, len(sys.argv)):
+              s += " " + sys.argv[i]
+              args.append(sys.argv[i])
+            args.append(filename)
+            print(s)
+            if process: process.kill()
+            process = subprocess.Popen(args)
+          print('Remove cart and insert next cart to dump\n')
+except:
+  if process: process.kill()
--- readme.md
+++ readme.md
@@ -12,21 +12,35 @@ You can solder male to male jumper wires directly to the pads or cut off one of 
 The dumper tool is a python script. To run it, open a commandline window. When no Python is installed on your computer (or you don't know) enter:
 
 ```
-dump2600p comport
+dump2600p comport [stella][arguments]
 ```
 
 or when Python is installed with pySerial on your computer, you can run the script directly by entering:
 
 ```
-dump2600p.py comport
+dump2600p.py comport [stella][arguments]
 ```
 
 comport is the name of the serial port of the FTDI cable. To find out which serial port is used by the FTDI cable, go to device manager and check under ports which com port is added when you insert the cable into the computer.
 
+[stella] is the full path to the Stella executable. When specified the Stella emulator will be launched with the dumped rom.
+
+[arguments] arguments passed on to the Stella emulator. The arguments are the same as you would normally use with Stella. For example to run Stella full screen you can use:
+
+```
+-fullscreen 1
+```
+
 After entering one of the above commands the dumper will run continiously and wait for the 2600+ to detect a cart.
 When a cart is succesfully detected it is dumped and saved to the romdump directory located in the same folder as the script.
 Once a cart is dumped you can pull the cart and insert another cart to dump (hot swap) or power off before inserting another cart and switching power back on.
-To exit the dumper press CTRL+C or close the window.
+To exit the dumper press CTRL+C (windows) or CTRL+Z (other) or close the window.
+
+When the path to the Stella emulator was specified. Your computer just functions as a 2600+. Each time you swap a cart Stella is run.
+The only difference is that Stella keeps running the same cart until a new cart is detected.
+
+If you power up the 2600+ with the B-W / Color switch in the B-W positon and Game reset pulled down. The 2600+ will function as a Joystick to USB adapter and a rom dumper.
+In this mode you can have the 2600+ experience but by using the Stella emulator on your computer.
 
 Notes:
 - The script should also work on Linux and OSX. Python3 with PySerial must be installed and the script should be run from the terminal.
